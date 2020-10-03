2ff0bf0493523887f12cfd8b094f6353a1c05b30
ryuga <vaishnav2353@gmail.com>
2020-10-03 11:31:04 +0530

    flask commit

--- app.py
+++ app.py
@@ -45,12 +45,6 @@ def register():
             return render_template("accounts/register.html", msg="Passwords doesn't match!")
 
 
-
-
 @app.route('/logout')
 def logout():
     return redirect(url_for('login'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
